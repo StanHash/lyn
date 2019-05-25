4e89a4a151340647dbeb5c3cda6f34d1d6d44709
Stan H <deindall@gmail.com>
2019-05-25 13:36:00 +0200

    print usage on arg error

--- main.cpp
+++ main.cpp
@@ -5,64 +5,83 @@
 #include "core/event_object.h"
 #include "util/hex_write.h"
 
-int do_diff(int argc, const char* const* argv) {
-	if (argc != 2) {
-		std::cerr << "[lyn diff] ERROR: 2 files required for compare" << std::endl;
-		return 1;
-	}
-
-	try {
-		lyn::event_object baseObject;
-		lyn::event_object otherObject;
-
-		baseObject.append_from_elf(argv[0]);
-		otherObject.append_from_elf(argv[1]);
-
-		struct sym_diff_data {
-			std::string baseName;
-			std::string otherName;
-		};
-
-		std::map<unsigned, sym_diff_data> symMap;
-
-		for (auto& sym : baseObject.absolute_symbols())
-			symMap[sym.offset].baseName = sym.name;
-
-		for (auto& sym : otherObject.absolute_symbols())
-			symMap[sym.offset].otherName = sym.name;
-
-		for (auto& pair : symMap) {
-			auto  address  = pair.first;
-			auto& nameDiff = pair.second;
-
-			if (nameDiff.baseName == nameDiff.otherName)
-				continue;
-
-			if (nameDiff.baseName.empty())
-				std::cout << "+" << util::make_hex_string("$", address) << " " << nameDiff.otherName << std::endl;
-
-			else if (nameDiff.otherName.empty())
-				std::cout << "-" << util::make_hex_string("$", address) << " " << nameDiff.baseName << std::endl;
-
-			else
-				std::cout << ">" << util::make_hex_string("$", address) << " " << nameDiff.baseName << " " << nameDiff.otherName << std::endl;
-		}
-	} catch (const std::exception& e) {
-		std::cerr << "[lyn diff] ERROR: " << e.what() << std::endl;
-		return 1;
-	}
-
-	return 0;
+void print_usage(std::ostream& out)
+{
+	out << "lyn usage:" << std::endl;
+	out << "  lyn <object>... [-[no]link] [-[no]longcalls] [-[no]temp] [-[no]hook] [-raw]" << std::endl;
+	out << "  lyn diff <old object> <new object>" << std::endl;
 }
 
-int main(int argc, char** argv) {
+int do_diff(int argc, const char* const* argv)
+{
+   if (argc != 2)
+   {
+	   print_usage(std::cerr);
+	   return 1;
+   }
+
+   try
+   {
+	   lyn::event_object baseObject;
+	   lyn::event_object otherObject;
+
+	   baseObject.append_from_elf(argv[0]);
+	   otherObject.append_from_elf(argv[1]);
+
+	   struct sym_diff_data
+	   {
+		   std::string baseName;
+		   std::string otherName;
+	   };
+
+	   std::map<unsigned, sym_diff_data> symMap;
+
+	   for (auto& sym : baseObject.absolute_symbols())
+		   symMap[sym.offset].baseName = sym.name;
+
+	   for (auto& sym : otherObject.absolute_symbols())
+		   symMap[sym.offset].otherName = sym.name;
+
+	   for (auto& pair : symMap)
+	   {
+		   auto  address  = pair.first;
+		   auto& nameDiff = pair.second;
+
+		   if (nameDiff.baseName == nameDiff.otherName)
+			   continue;
+
+		   if (nameDiff.baseName.empty())
+			   std::cout << "+" << util::make_hex_string("$", address) << " " << nameDiff.otherName << std::endl;
+
+		   else if (nameDiff.otherName.empty())
+			   std::cout << "-" << util::make_hex_string("$", address) << " " << nameDiff.baseName << std::endl;
+
+		   else
+			   std::cout << ">" << util::make_hex_string("$", address) << " " << nameDiff.baseName << " " << nameDiff.otherName << std::endl;
+	   }
+   }
+   catch (const std::exception& e)
+   {
+	   std::cerr << "[lyn diff] ERROR: " << e.what() << std::endl;
+	   return 1;
+   }
+
+   return 0;
+}
+
+int main(int argc, char** argv)
+{
 	if (argc < 2)
+	{
+		print_usage(std::cerr);
 		return 1;
+	}
 
 	if (!std::strcmp(argv[1], "diff"))
 		return do_diff(argc - 2, argv + 2);
 
-	struct {
+	struct
+	{
 		bool doLink          = true;
 		bool longCall        = false;
 		bool applyHooks      = true;
@@ -71,40 +90,77 @@ int main(int argc, char** argv) {
 
 	std::vector<std::string> elves;
 
-	for (int i = 1; i < argc; ++i) {
+	for (int i = 1; i < argc; ++i)
+	{
 		std::string argument(argv[i]);
 
 		if (argument.size() == 0)
 			continue;
 
-		if (argument[0] == '-') { // option
-			if (argument        == "-nolink") {
-				options.doLink         = false;
-			} else if (argument == "-link") {
-				options.doLink         = true;
-			} else if (argument == "-longcalls") {
-				options.longCall       = true;
-			} else if (argument == "-nolongcalls") {
-				options.longCall       = false;
-			} else if (argument == "-raw") {
-				options.doLink         = false;
-				options.longCall       = false;
-				options.applyHooks     = false;
-			} else if (argument == "-temp") {
+		if (argument[0] == '-') // option
+		{
+			if (argument  == "-nolink")
+			{
+				options.doLink = false;
+				continue;
+			}
+
+			if (argument == "-link")
+			{
+				options.doLink = true;
+				continue;
+			}
+
+			if (argument == "-longcalls")
+			{
+				options.longCall = true;
+				continue;
+			}
+
+			if (argument == "-nolongcalls")
+			{
+				options.longCall = false;
+				continue;
+			}
+
+			if (argument == "-raw")
+			{
+				options.doLink = false;
+				options.longCall = false;
+				options.applyHooks = false;
+				continue;
+			}
+
+			if (argument == "-temp")
+			{
 				options.printTemporary = true;
-			} else if (argument == "-notemp") {
+				continue;
+			}
+
+			if (argument == "-notemp")
+			{
 				options.printTemporary = false;
-			} else if (argument == "-hook") {
-				options.applyHooks     = true;
-			} else if (argument == "-nohook") {
-				options.applyHooks     = false;
+				continue;
+			}
+
+			if (argument == "-hook")
+			{
+				options.applyHooks = true;
+				continue;
+			}
+
+			if (argument == "-nohook")
+			{
+				options.applyHooks = false;
+				continue;
 			}
 		} else { // elf
 			elves.push_back(std::move(argument));
 		}
 	}
 
-	try {
+	try
+	{
 		lyn::event_object object;
 
 		for (auto& elf : elves)
@@ -124,8 +180,10 @@ int main(int argc, char** argv) {
 
 		object.cleanup();
 
-		if (options.applyHooks) {
-			for (auto& hook : object.get_hooks()) {
+		if (options.applyHooks)
+		{
+			for (auto& hook : object.get_hooks())
+			{
 				lyn::event_object temp;
 
 				std::cout << "PUSH" << std::endl;
@@ -139,7 +197,9 @@ int main(int argc, char** argv) {
 		}
 
 		object.write_events(std::cout);
-	} catch (const std::exception& e) {
+	}
+	catch (const std::exception& e)
+	{
 		std::cerr << "[lyn] ERROR: " << e.what() << std::endl;
 		return 1;
 	}
